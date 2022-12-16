edd958bfa9269eb8caada6be264a96793e116c35
Cameron Hunter <hello@cameronhunter.co.uk>
2022-12-16 10:13:10 -0800

    WIP
    Initial implementation of finding related tests from tags

--- test/lib.test.ts
+++ test/lib.test.ts
@@ -1,5 +1,16 @@
 import { DependencyResolver } from '../src/lib';
 
+test('Find related tests', async () => {
+  const resolver = await DependencyResolver.create('jest.config.cjs', { cwd: './test/__fixtures__' });
+
+  const changedFiles = ['components/keyboard.js'];
+  const relatedTests = await resolver.resolveTests(changedFiles);
+
+  expect(relatedTests).toEqual(
+    new Set(['__functional__/keyboard.test.js', '__functional__/search.test.js', '__functional__/smoke.test.js'])
+  );
+});
+
 test('tags', async () => {
   const resolver = await DependencyResolver.create('jest.config.cjs', { cwd: './test/__fixtures__' });
 
@@ -15,16 +26,5 @@ test('tags', async () => {
 
   expect(await resolver.resolveInverseTags(['components/text.js', 'components/image.js'])).toEqual(
     new Set(['bob', 'browse', 'button', 'edp', 'keyboard', 'mdp', 'menu', 'search', 'text', 'boxshot', 'image'])
-  );
-});
-
-test('Find related tests', async () => {
-  const resolver = await DependencyResolver.create('jest.config.cjs', { cwd: './test/__fixtures__' });
-
-  const changedFiles = ['components/keyboard.js'];
-  const relatedTests = await resolver.resolveTests(changedFiles);
-
-  expect(relatedTests).toEqual(
-    new Set(['__functional__/keyboard.test.js', '__functional__/search.test.js', '__functional__/smoke.test.js'])
   );
 });
